05a10032215ee5ae48a867d2be608611c243e3b1
alsey89 <101123930+alsey89@users.noreply.github.com>
2024-03-04 22:27:43 +0800

    chore: update favicon path

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
         {
           rel: "icon",
           type: "image/x-icon",
-          href: "/favicon.ico",
+          href: process.env.FAVICON_PATH || "/favicon.ico",
         },
       ],
     },
