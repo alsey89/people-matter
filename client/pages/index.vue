6643518c76752df8d3b54bc23f181ecd5dd57fdb
alsey89 <101123930+alsey89@users.noreply.github.com>
2024-03-05 20:36:21 +0800

    chore: remove deploy files, add preview label

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -20,7 +20,7 @@
     <div class="flex-1 space-y-4 p-8 pt-6">
       <div class="flex items-center justify-between space-y-2">
         <h2 class="text-3xl font-bold tracking-tight">
-          Dashboard
+          Dashboard Preview <span class="text-primary font-bold"> [Sample Data] </span>
         </h2>
         <div class="flex items-center space-x-2">
           <AppHomeDateRangePicker />
