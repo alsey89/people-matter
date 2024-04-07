34e8e9b850d7c70ddb719b479d96f3bfabee4325
alsey89 <101123930+alsey89@users.noreply.github.com>
2024-04-07 17:36:27 +0800

    chore: set up server and logger module, refactor main

--- server/go.mod
+++ server/go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/spf13/viper v1.17.0
 	github.com/swaggo/echo-swagger v1.4.1
 	github.com/swaggo/swag v1.16.2
+	go.uber.org/fx v1.21.0
+	go.uber.org/zap v1.26.0
 	golang.org/x/crypto v0.17.0
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
@@ -47,8 +49,8 @@ require (
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/dig v1.17.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,121 +1,17 @@
 package main
 
 import (
-	"net/http"
+	"verve-hrms/pkg/logger"
+	"verve-hrms/pkg/server"
 
-	echojwt "github.com/labstack/echo-jwt/v4"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	echoSwagger "github.com/swaggo/echo-swagger"
-
-	"github.com/spf13/viper"
-
-	_ "verve-hrms/docs"
-	"verve-hrms/internal/auth"
-	"verve-hrms/internal/company"
-	"verve-hrms/internal/job"
-	"verve-hrms/internal/user"
-	"verve-hrms/setup"
+	"go.uber.org/fx"
 )
 
-// @title Verve HRMS API
-// @version 1.0
-// @description This server provides APIs for the Verve HRMS application
-
-// @contact.name alsey89
-// @contact.email phyokyawsoe89@gmail.com
-
-// @license.name GPL 3.0
-// @license.url https://www.gnu.org/licenses/gpl-3.0.en.html
-
-// @host localhost:3001
-// @BasePath /api/v1
-
 func main() {
-	e := echo.New()
+	app := fx.New(
+		logger.Module(),
+		server.Module("server"),
+	)
 
-	//! Config
-	setup.InitConfig()
-
-	//! Load DB
-	client := setup.GetClient()
-	//! Middleware
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{viper.GetString("CLIENT_URL")},
-		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-		AllowCredentials: true,
-		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
-	}))
-	if viper.GetBool("IS_PRODUCTION") {
-		e.Use(middleware.Secure())
-		e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-			TokenLookup:    "cookie:_csrf",
-			CookiePath:     "/",
-			CookieDomain:   viper.GetString("PRODUCTION_DOMAIN"),
-			CookieSecure:   true,
-			CookieHTTPOnly: true,
-			CookieSameSite: http.SameSiteStrictMode,
-		}))
-	} else {
-		// e.Use(middleware.Secure()) //! this is not needed for development
-		e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-			TokenLookup:    "cookie:_csrf",
-			CookiePath:     "/",
-			CookieDomain:   "localhost",
-			CookieSecure:   false,
-			CookieHTTPOnly: true,
-			CookieSameSite: http.SameSiteLaxMode,
-		}))
-	}
-	e.Use(echojwt.WithConfig(echojwt.Config{
-		Skipper: func(c echo.Context) bool {
-			if c.Request().URL.Path == "/api/v1/auth/signin" ||
-				c.Request().URL.Path == "/api/v1/auth/signup" ||
-				c.Request().URL.Path == "/api/v1/auth/signout" ||
-				c.Request().URL.Path == "/api/v1/auth/csrf" ||
-				c.Request().URL.Path == "/api/v1/auth/password/reset" ||
-				c.Request().URL.Path == "/swagger" {
-				return true
-			}
-			return false
-		},
-		SigningKey:    []byte(viper.GetString("JWT_SECRET")),
-		SigningMethod: "HS256",
-		TokenLookup:   "cookie:jwt",
-	}))
-	e.Use(middleware.Logger())
-	//! Swagger
-	e.Static("/swagger", "docs")
-	e.GET("/swagger/*", echoSwagger.WrapHandler)
-
-	//! Domains
-	//* Instantiate User Domain
-	userRepository := user.NewUserRepository(client)
-	userService := user.NewUserService(userRepository)
-	userHandler := user.NewUserHandler(userService)
-	//* Instantiate Auth Domain
-	authService := auth.NewAuthService(userService)
-	authHandler := auth.NewAuthHandler(authService)
-	//*Instantiate Job Domain
-	jobRepository := job.NewJobRepository(client)
-	// jobService := job.NewJobService(jobRepository)
-	// jobHandler := job.NewJobHandler(jobService)
-	//*Instantiate Company Domain
-	companyRepository := company.NewCompanyRepository(client)
-	companyService := company.NewCompanyService(companyRepository, jobRepository)
-	companyHandler := company.NewCompanyHandler(companyService)
-
-	//! Routes
-	//* Auth Routes
-	authRoutes := e.Group("api/v1/auth")
-	auth.SetupAuthRoutes(authRoutes, authHandler)
-	//* User Routes
-	userRoutes := e.Group("api/v1/user")
-	user.SetupUserRoutes(userRoutes, userHandler)
-	//* Company Routes
-	companyRoutes := e.Group("api/v1/company")
-	company.SetupCompanyRoutes(companyRoutes, companyHandler)
-
-	//! START THE SERVER
-	e.Logger.Fatal(e.Start(":" + viper.GetString("SERVER_PORT")))
+	app.Run()
 }
